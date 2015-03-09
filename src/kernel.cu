5773c71d9446227fcc66a8c77822c911f14b3c06
jerbae@gmail.com <jerbae@gmail.com>
2015-03-09 16:29:29 +0100

    staging

---
+++ src/kernel.cu
@@ -0,0 +1,18 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+cudaError_t cuda();
+
+__global__ void kernel()
+{
+  
+}
+
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t cuda()
+{
+    cudaError_t cudaStatus;
+    return cudaStatus;
+}
