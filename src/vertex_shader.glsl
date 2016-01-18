b426fd6f4ae5854b7e8df58119271135fece13d4
jerbae@gmail.com <jerbae@gmail.com>
2016-01-18 15:11:12 +0100

    Working OpenGL quad version, lots of insight gained

--- src/vertex_shader.glsl
+++ src/vertex_shader.glsl
@@ -1,6 +1,12 @@
 #version 130
+
+// beter: vertices in VBO rammen en hier - layout(location = 0) in vec4 position;
+
 void main(void)
 {
 	gl_Position = gl_Vertex;
 	gl_TexCoord[0].xy = gl_MultiTexCoord0.xy;
 }
+
+// gl_Position is a builtin
+// gl_TexCoord is deprecated
