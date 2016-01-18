3f5288dfc286c16f54a014458499f7f7529596ac
jerbae@gmail.com <jerbae@gmail.com>
2016-01-18 16:15:21 +0100

    Use core profile

--- src/main_basic_gl.cpp
+++ src/main_basic_gl.cpp
@@ -25,6 +25,9 @@ GLfloat vertices[] = {
 	-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // Bottom Left
 	-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f // Top Left 
 };
+
+// you can also put positions and colors these in seperate VBO's
+
 GLuint indices[] = {  // Note that we start from 0!
 	0, 1, 3,  // First Triangle
 	1, 2, 3   // Second Triangle
@@ -72,6 +75,7 @@ void keyboardfunc(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 bool initGL(){
+	glewExperimental = GL_TRUE;
 	glewInit();
 	glViewport(0, 0, width, height); // viewport for x,y to normalized device coordinates transformation
 	return true;
@@ -79,6 +83,10 @@ bool initGL(){
 
 bool initGLFW(){
 	if (!glfwInit()) exit(EXIT_FAILURE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	window = glfwCreateWindow(width, height, "The Simplest OpenGL Quad", NULL, NULL);
 	if (!window){ glfwTerminate(); exit(EXIT_FAILURE); }
 	glfwMakeContextCurrent(window);
@@ -90,6 +98,13 @@ bool initGLFW(){
 int main(int argc, char *argv[]) {
 	initGLFW();
 	initGL();
+
+	printf("%i \n", glfwGetWindowAttrib(window, GLFW_OPENGL_PROFILE));
+	printf("%i \n", GLFW_OPENGL_COMPAT_PROFILE);
+	printf("%i \n", GLFW_OPENGL_CORE_PROFILE);
+
+	printf("%s\n", glGetString(GL_VERSION));
+	printf("%s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
 	// Generate buffers
 	glGenVertexArrays(1, &VAO);
